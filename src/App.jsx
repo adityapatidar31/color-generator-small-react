79aaec05a58c9fe8357406d6aa766b2c13b02b35
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-12 22:39:01 +0530

    generate tins and shads of the hardCoded Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Form from "./Form";
-
+import Values from "values.js";
 function App() {
+  const [colors, setColors] = useState([new Values("#f15025").all(10)]);
+
   return (
     <div>
       <Form />
