8bdba625525910aa1206f3ef9f2b4d2ddadad8e2
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-12 22:40:33 +0530

    pass the colors to colorList and integrate it with App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Form from "./Form";
 import Values from "values.js";
+import ColorList from "./ColorList";
 function App() {
   const [colors, setColors] = useState([new Values("#f15025").all(10)]);
 
   return (
     <div>
       <Form />
+      <ColorList colors={colors} />
     </div>
   );
 }
