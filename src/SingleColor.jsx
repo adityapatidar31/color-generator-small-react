2503e031d8d313760541468764a9b5b5a89d262c
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-12 22:46:22 +0530

    partially completed the singleColor Page

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,5 +1,25 @@
-function SingleColor() {
-  return <div>SingleColor SingleColor</div>;
-}
+/* eslint-disable react/prop-types */
 
+const SingleColor = ({ index, color }) => {
+  const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
+};
 export default SingleColor;
